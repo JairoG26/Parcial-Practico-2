dad48fdff22e7f1b546560388167f47a64b3eea6
Jairo Garavito <ja.garavito@uniandes.edu.co>
2023-11-24 13:56:14 -0500

    Preparado para el parcial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SerieModule } from './serie/serie.module';
+import { DinosaurioModule } from './dinosaurio/dinosaurio.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    SerieModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
